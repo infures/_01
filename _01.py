ce84363a57abd5938d0408e9b60bbc8b4ba5022a
infures <amarr1337228007@mail.ru>
2023-07-03 19:23:33 +0300

    полиндром

--- _01.py
+++ _01.py
@@ -1,1 +1,10 @@
+#<<<<<<< HEAD
 #git config --global user.name
+#=======
+slovo = str(input('введите слово полиндром'))
+a = slovo[::-1]
+if slovo == a:
+    print("слово полиндром")
+else:
+    print("слово полиндром")
+#>>>>>>> 3c5d13f (полиндром)
